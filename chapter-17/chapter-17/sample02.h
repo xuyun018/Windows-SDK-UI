adc42557bf00d53b1a12906e748549376df10c25
xuyun018 <55885541+xuyun018@users.noreply.github.com>
2024-01-03 00:33:12 +0800

    Add files via upload

---
+++ chapter-17/chapter-17/sample02.h
@@ -0,0 +1,18 @@
+#ifndef SAMPLE02_H
+#define SAMPLE02_H
+
+#define IDM_NEW			100
+#define IDM_OPEN		200
+#define IDM_EXIT		300
+//DIALOG BOX
+#define IDC_STATIC    3000
+#define IDC_EDIT1		4000
+#define IDC_CHECK1    4010
+#define IDC_CHECK2    4020
+#define IDC_RADIO1    4030
+#define IDC_RADIO2    4040
+//TIMER
+#define ID_MYTIMER    32767
+
+#endif
+
